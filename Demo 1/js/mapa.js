ec2d1f518789363a9cad2bc0f4ae5bc1cbf9f87b
Joao Grassi <joaop.grassi@gmail.com>
2016-01-10 11:21:42 -0200

    Added comments to js file

--- Demo 1/js/mapa.js
+++ Demo 1/js/mapa.js
@@ -1,10 +1,12 @@
-$(function () {        
+$(function () {
 		
+		//Options of the Map
 		var mapOptions = {
           center: new google.maps.LatLng(-17, -50),
           zoom: 5,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };	
+          mapTypeId: google.maps.MapTypeId.ROADMAP //type of the map
+        };
 		
+		//Creates the map
         var map = new google.maps.Map(document.getElementById("mapa"), mapOptions);	  
 });
